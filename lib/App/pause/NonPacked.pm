453a07d7d365424d334df9df3b64dd8ee88782ca
perlancar (@pc-office) <perlancar@gmail.com>
2015-07-07 16:43:46 +0700

    Initial commit

---
+++ lib/App/pause/NonPacked.pm
@@ -0,0 +1,17 @@
+package App::pause::NonPacked;
+
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: A CLI for PAUSE (nonpacked version)
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+L<App::pause>
+
+L<pause-nonpacked>
