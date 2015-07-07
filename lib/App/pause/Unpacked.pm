679067d47d6dfce089c4b1fe1f4bc08fc73619d1
perlancar (@pc-office) <perlancar@gmail.com>
2015-07-07 17:48:44 +0700

    Rename dist from App-pause-NonPacked to App-pause-Unpacked

---
+++ lib/App/pause/Unpacked.pm
@@ -0,0 +1,17 @@
+package App::pause::Unpacked;
+
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: A CLI for PAUSE (unpacked version)
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+L<App::pause>
+
+L<pause-unpacked>
